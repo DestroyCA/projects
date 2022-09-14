cbe7104991c43c990db209b9a21c1954d9d52c96
DestroyCA <destroy.citya@gmail.com>
2022-09-15 01:02:17 +0700

    Create MainActivity.kt

---
+++ MainActivity.kt
@@ -0,0 +1,11 @@
+package com.practicum.volume
+
+fun main() {
+    val length: Int = 9     //длина
+    val width: Int = 6      //ширина
+    val height: Int = 3     //высота
+    val volume: Int = (length * width * height)
+    /* формула расчёта объёма
+    Перемножение всех значений */
+    println(volume)     //результат 162
+}
